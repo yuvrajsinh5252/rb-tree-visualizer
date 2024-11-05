cfab1688dd5210d1f4a0bcdd629b36e85a937bd3
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-11-05 10:29:57 +0530

    stuff updated

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "all",
+  content: [
+    // Include all Rust, HTML, and CSS files in the src directory
+    "./src/**/*.{rs,html,css}",
+    // Include all HTML files in the output (dist) directory
+    "./dist/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
